6f9c643d1df648d04c2a6ed5cf1f35c948527382
Abinaya1203 <94049206+Abinaya1203@users.noreply.github.com>
2024-03-21 20:19:59 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,56 @@
+//public class Method{//without arguments
+//    void addition(){
+//        int a=10;
+//        int b=12;
+//        int c=a+b;
+//        System.out.println("Result " +c);
+//    }
+//
+//    public static void main(String[] args) {
+//        Method caller=new Method();
+//        caller.addition();
+//
+//    }
+//}
+//  with arguments
+//public class Method {
+//    void sub(int a,int b){
+//        int c=a-b;
+//        System.out.println("Result "+c);
+//    }
+//
+//    public static void main(String[] args) {
+//        Method caller=new Method();
+//        caller .sub(10,5);
+//
+//    }
+//}
+//public class Method {
+//    void Str(String a,String b,String c) {
+//        System.out.println("Result " + a + b + c);
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Method caller=new Method();
+//        caller.Str("x","y","z");
+//    }
+//}// if we dont want the particular method remove the caller
+// GETTING THE RETURN VALUE
+    public class Method {
+        int add(int a, int b) {
+            int c = a + b;
+
+            return c;
+
+
+        }
+
+        public static void main(String[] args) {
+            Method caller = new Method();
+
+           int d= caller.add(10, 5);// in this we assign the value for the integer d
+            System.out.println("Result" + d);
+
+        }
+    }
